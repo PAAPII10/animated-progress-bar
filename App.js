0e618fc960e44137fb8617c396923fae597c65b9
PAAPII10 <vishalchaurasia250@gmail.com>
2023-04-05 17:05:42 +0530

    animated-progress

--- App.js
+++ App.js
@@ -1,22 +1,74 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Animated } from "react-native";
 
+const Progress = ({ step, steps, height }) => {
+  const [width, setWidth] = React.useState(0);
+  const animatedValue = React.useRef(new Animated.Value(-1000)).current;
+  const reactive = React.useRef(new Animated.Value(-1000)).current;
+
+  React.useEffect(() => {
+    Animated.timing(animatedValue, {
+      toValue: reactive,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  }, []);
+
+  React.useEffect(() => {
+    reactive.setValue(-width + (width * step) / steps);
+  }, [step, width]);
+  return (
+    <>
+      <Text style={{ fontSize: 12, fontWeight: "900", marginBottom: 8 }}>
+        {step}/{steps}
+      </Text>
+      <View
+        onLayout={(e) => {
+          const newWidth = e.nativeEvent.layout.width;
+          setWidth(newWidth);
+        }}
+        style={{
+          height,
+          backgroundColor: "rgba(0,0,0,0.1)",
+          borderRadius: height,
+          overflow: "hidden",
+        }}
+      >
+        <Animated.View
+          style={{
+            height,
+            width: "100%",
+            backgroundColor: "red",
+            borderRadius: height,
+            left: 0,
+            top: 0,
+            transform: [
+              {
+                translateX: animatedValue,
+              },
+            ],
+          }}
+        />
+      </View>
+    </>
+  );
+};
 export default function App() {
+  const [index, setIndex] = React.useState(0);
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((index + 1) % (10 + 1));
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [index]);
+
   return (
     <View style={styles.container}>
       <StatusBar hidden />
-      <Text style={{ fontSize: 42 }}>❤️</Text>
-      <Text
-        style={{
-          marginTop: 10,
-          fontWeight: "800",
-          fontSize: 16,
-        }}
-      >
-        Expo
-      </Text>
-      <Text style={{ fontStyle: "italic", fontSize: 12 }}>(expo.io)</Text>
+      <Progress step={index} steps={10} height={20} />
     </View>
   );
 }
@@ -25,7 +77,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    padding: 20,
   },
 });
